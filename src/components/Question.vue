85a40209f66fb67657c6daa6141cc7d3df3bfb7d
Charmaine <charmaine.hym@gmail.com>
2022-04-26 18:49:29 +0800

    saving before pop

---
+++ src/components/Question.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="quiz.show">
+  <h1>{{ quiz.question }}</h1>
+  <div class="choices" v-for="choice in quiz.choices" :key="choice">
+      <button>{{ choice }}</button>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['quiz'],
+    data(){
+        return{
+            answered: 1
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{ answers }}</p>
+</template>
+
+<script>
+export default {
+    props: ['answers']
+
+}
+</script>
+
+<style>
+
+</style>
